33ec500e037c35836d93490971aceed553cdd0fd
nix.per <nikkigautam562@gmail.com>
2025-02-21 19:57:18 +0530

    Added second-hand add item frontend and connectivity

--- controllers/rentingController.js
+++ controllers/rentingController.js
@@ -3,7 +3,15 @@
 exports.getHome = (req, res, next) => {
   res.render("rentals/rentalHome", {
     pageTitle: "Renting",
-    path: "/home",
+    path: "/rental",
+  });
+};
+
+//add product form
+exports.getAddProduct = (req, res, next) => {
+  res.render("rentals/post-product", {
+    pageTitle: "Post Product",
+    path: "/rental/post",
   });
 };
 
--- routes/rentingRoutes.js
+++ routes/rentingRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const rentingController = require("../controllers/rentingController");
 
 router.get("/", rentingController.getHome);
+router.get("/post", rentingController.getAddProduct);
+// router.post("/post", secondHandController.postAddProduct);
 
 module.exports = router;
