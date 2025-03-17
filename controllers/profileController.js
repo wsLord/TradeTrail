3488f63078831cc8015c96fe0105e4507f60a290
nix.per <nikkigautam562@gmail.com>
2025-03-17 15:19:59 +0530

    Added profile UI and hover-tooltip

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,16 +1,22 @@
 // controllers/profileController.js
 exports.getProfile = (req, res) => {
     const userProfile = {
-        name: "Kanye West",
-        email: "kanye@aweso.me",
-        city: "San Francisco",
-        state: "CA",
-        country: "USA",
-        phone: "+1 (415) 655-17-10",
+        name: "Name Surname",
+        email: "name@dummy.com",
+        city: "New Delhi",
+        state: "Delhi",
+        country: "India",
+        phone: "+91 415-655-17-10",
         rating: "4.5",
-        badge: "new user"
-    };
+        badge: "new user",
 
+        badges: [
+            '/badges/badgeone.png',
+            '/badges/badgetwo.png',
+            '/badges/badgethree.png'
+        ]
+    };
+    
     res.render("profile", { 
         user: userProfile,
         activePage: "profile"  // Added activePage for navbar highlighting
