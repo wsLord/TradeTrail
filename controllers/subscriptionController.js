c2c179fe95bd45024bc559b2797b320ce6535ad9
Leshya09 <leshyaj@gmail.com>
2025-03-30 22:54:32 +0530

    done

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -151,6 +151,7 @@ exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Ott.findById(prodId)
     .populate("seller", "fullName")
+    .populate("winner", "fullName")
     .populate({
       path: "bids",
       populate: { path: "bidder", select: "fullName" },
